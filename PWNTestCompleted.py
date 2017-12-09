49d82cbf638797b862f0a996b5faee9878b6be57
eliecer <eliecerecology@gmail.com>
2017-12-09 17:44:41 +0200

    all working function working

--- PWNTestCompleted.py
+++ PWNTestCompleted.py
@@ -2,74 +2,67 @@ import RPi.GPIO as GPIO
 import time
 import tkinter 
 
+
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 pin = 7 # forward right wheel
 pin2 = 11 #backward right wheel
 pin3 = 12 #forward left wheel
 pin4 = 13 #backward left wheel
 
 def init():
-	GPIO.setup(pin, GPIO.OUT)
-	GPIO.setup(pin2, GPIO.OUT)
-	GPIO.setup(pin3, GPIO.OUT)
-	GPIO.setup(pin4, GPIO.OUT)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.setup(pin2, GPIO.OUT)
+    GPIO.setup(pin3, GPIO.OUT)
+    GPIO.setup(pin4, GPIO.OUT)
 
 def forward(tf):
-	init()
-	p = GPIO.PWM(pin, 50)
-	p1 = GPIO.PWM(pin3, 50)
-	p.start(60)
-	p1.start(60)
-	time.sleep(tf)
-	p.start(0)
-	p1.start(0)
+    init()
+    p = GPIO.PWM(pin, 50)
+    p1 = GPIO.PWM(pin3, 50)
+    p.start(60)
+    p1.start(60)
+    time.sleep(tf)
+    p.start(0)
+    p1.start(0)
 
-def backwards(tf):
-	init()
-	q= GPIO.PWM(pin2, 50)
-	q1= GPIO.PWM(pin4, 50)
-	q.start(60)
-	q1.start(60)
-	time.sleep(tf)
-	q.start(0)
-	q1.start(0)
+def backward(tf):
+    init()
+    q= GPIO.PWM(pin2, 50)
+    q1= GPIO.PWM(pin4, 50)
+    q.start(60)
+    q1.start(60)
+    time.sleep(tf)
+    q.start(0)
+    q1.start(0)
 
 def turn_right(tf):
-	init()
-	p = GPIO.PWM(pin, 50)
-	q1= GPIO.PWM(pin4, 50)
-	p.start(60)
-	q1.start(60)
-	time.sleep(tf)
-	p.start(0)
-	q1.start(0)
-	
-	
-	
-def turn_left(tf):
-	init()
-	p1 = GPIO.PWM(pin3, 50)
-	q1= GPIO.PWM(pin2, 50)
-    p1.start(60)
-	q1.start(60)
-	time.sleep(tf)
-	p1.start(0)
-	q1.start(0)
-	
-# Stopping the GPIOs == GPIO.output(pin, False)
-def stop()
-	p.start(0)
-	p1.start(0)
-	q.start(0)
-	q1.start(0)
+    init()
+    p = GPIO.PWM(pin, 50)
+    q1= GPIO.PWM(pin4, 50)
+    p.start(60)
+    q1.start(60)
+    time.sleep(tf)
+    p.start(0)
+    q1.start(0)
 
-def key_input(event):
-	
+def turn_left(tf):
+    init()
+    p1 = GPIO.PWM(pin3, 50)
+    q1= GPIO.PWM(pin2, 50)
+    p1.start(60)
+    q1.start(60)
+    time.sleep(tf)
+    p1.start(0)
+    q1.start(0)
+
+#def key_input(event):
+
 forward(2)
 backward(2)
 turn_right(2)
 turn_left(2)
-stop()
+
 
 print("test completed!")
 
