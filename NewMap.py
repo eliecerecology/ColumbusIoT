9c019a5de1b560e4cc38aae878d5790507e84527
eliecerecology <eliecerecology@gmail.com>
2018-01-01 16:52:42 +0200

    Redone Map

--- NewMap.py
+++ NewMap.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import math
 from pygame.math import Vector2
 import time
 
@@ -20,7 +21,11 @@ class Bullet(pg.sprite.Sprite):
         #1 set the angle of the image!
         self.image = pg.transform.rotate(BULLET_IMAGE, -angle)
         #2. get_rect gives the position
-        self.rect = self.image.get_rect(center=pos)
+        self.rect = self.image.get_rect(center=pos) #(center=pos)
+
+        angle_rad = math.radians(angle)
+        self.velocity_x = speed * math.cos(angle_rad)
+        self.velocity_y = speed * math.sin(angle_rad)
 
         # To apply an offset to the start position,
         # create another vector and rotate it as well.
@@ -32,10 +37,12 @@ class Bullet(pg.sprite.Sprite):
         # Rotate the direction vector (1, 0) by the angle.
         # Multiply by desired speed.
         self.velocity = Vector2(1, 0).rotate(angle) * 9
-
+        
     def update(self): #siempre update the position
         self.pos += self.velocity  # Add velocity to pos to move the sprite. (2 components of this vector)
         self.rect.center = self.pos  # Update rect coords.
+
+        
 
         if not screen_rect.contains(self.rect):
             self.kill()
@@ -70,29 +77,30 @@ def main():
         elif keys[pg.K_d]:
             angle += 3
         elif keys[pg.K_w]:
-            print(pos)
+            #velocity_x += 3
+            #velocity_y += 3
         #if keys[pg.K_SPACE]:
             #bullet_group.add(Bullet(cannon.center, angle))
 
         # Rotate the cannon image.
-        angulos = list(range(0,45))
-        for idx, angle in enumerate(angulos):
-            print(angle)
-            rotated_cannon_img = pg.transform.rotate(cannon_img, -angle) #-val, -angle
-            cannon = rotated_cannon_img.get_rect(center=cannon.center)
-            time.sleep(0.3)
-            bullet_group.update()
+        #angulos = list(range(0,45))
+        #for idx, angle in enumerate(angulos):
+        #print(angle)
+        rotated_cannon_img = pg.transform.rotate(cannon_img, -angle) #-val, -angle
+        cannon = rotated_cannon_img.get_rect(center=cannon.center)
+        #time.sleep(0.3)
+        bullet_group.update()
 
                 # Draw
-            screen.fill((3, 6, 8))
-            screen.blit(rotated_cannon_img, cannon)
-            bullet_group.draw(screen)
+        screen.fill((3, 6, 8))
+        screen.blit(rotated_cannon_img, cannon)
+        bullet_group.draw(screen)
 
-            txt = FONT.render('angle rotation {:.1f}'.format(angle), True, (150, 150, 170))
-            screen.blit(txt, (10, 10))
-            pg.display.update()
+        txt = FONT.render('angle rotation {:.1f}'.format(angle), True, (150, 150, 170))
+        screen.blit(txt, (10, 10))
+        pg.display.update()
 
-            clock.tick(30)    
+        clock.tick(30)    
 
 if __name__ == '__main__':
     main()
--- NewMap2.py
+++ NewMap2.py
@@ -1,46 +1,88 @@
-import math
-import pygame
+
+import pygame as pg
+from pygame.math import Vector2
 
 
-pygame.init()
+pg.init()
+screen = pg.display.set_mode((640, 480))
+screen_rect = screen.get_rect()
 
-screen = pygame.display.set_mode((640, 480))
-clock = pygame.time.Clock()
-
-BULLET_IMAGE = pygame.Surface((20, 11), pygame.SRCALPHA)
-pygame.draw.polygon(BULLET_IMAGE, pygame.Color('aquamarine1'),
+FONT = pg.font.Font(None, 24)
+BULLET_IMAGE = pg.Surface((20, 11), pg.SRCALPHA)
+pg.draw.polygon(BULLET_IMAGE, pg.Color('aquamarine1'),
                 [(0, 0), (20, 5), (0, 11)])
 
 
-class Bullet(pygame.sprite.Sprite):
+class Bullet(pg.sprite.Sprite):
 
-    def __init__(self, x, y, angle, speed):
-        pygame.sprite.Sprite.__init__(self)
-        # Rotate the bullet image (negative angle because y-axis is flipped).
-        self.image = pygame.transform.rotate(BULLET_IMAGE, -angle)
-        self.rect = self.image.get_rect(center=(x, y))
-        angle = math.radians(angle)
-        self.speed_x = speed * math.cos(angle)
-        self.speed_y = speed * math.sin(angle)
+    def __init__(self, pos, angle):
+        super().__init__()
+        self.image = pg.transform.rotate(BULLET_IMAGE, -angle)
+        self.rect = self.image.get_rect(center=pos)
+        # To apply an offset to the start position,
+        # create another vector and rotate it as well.
+        self.angle_speed = 0 #<---new
+        offset = Vector2(pos).rotate(angle)
+        # Then add the offset vector to the position vector.
+        self.pos = Vector2(pos) + offset  # Center of the sprite.
+        # Rotate the direction vector (1, 0) by the angle.
+        # Multiply by desired speed.
+        self.velocity = Vector2(1, 0).rotate(angle) * 9
 
     def update(self):
-        self.rect.x += self.speed_x
-        self.rect.y += self.speed_y
+        self.pos += self.velocity  # Add velocity to pos to move the sprite.
+        self.rect.center += self.pos  # Update rect coords.
+        if not screen_rect.contains(self.rect):
+           self.kill()
+        self.direction.rotate_ip(self.angle_speed)
+        self.angle += self.angle_speed
+        
+def main():
+    a = 10
+    clock = pg.time.Clock()
+    cannon_img = pg.Surface((40, 20), pg.SRCALPHA)
+    cannon_img.fill(pg.Color('aquamarine3'))
+    cannon = cannon_img.get_rect(center=(320, 240))
+    angle = 0
+    bullet_group = pg.sprite.Group()  # Add bullets to this group.
 
-spr = pygame.sprite.Group()
-bullet = Bullet(10, 10, 60, 3)
-bullet2 = Bullet(10, 10, 30, 3)
-spr.add(bullet, bullet2)
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                # Left button fires a bullet from center with
+                # current angle. Add the bullet to the bullet_group.
+                if event.button == 1:
+                    bullet_group.add(Bullet(cannon.center, angle))
 
-play = True
-while play:
-    clock.tick(60)
-    for ev in pygame.event.get():
-        if ev.type == pygame.QUIT:
-            play = False
-    screen.fill((30,30,40))
-    spr.update()
-    spr.draw(screen)
-    pygame.display.flip()
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            angle -= 3
+        elif keys[pg.K_d]:
+            angle += 3
+        if keys[pg.K_SPACE]:
+            bullet_group.add(Bullet(cannon.center, angle))
+        elif keys[pg.K_w]:
+            cannon.angle_speed = 0.8
 
-pygame.quit()
+        # Rotate the cannon image.
+        rotated_cannon_img = pg.transform.rotate(cannon_img, -angle)
+        cannon = rotated_cannon_img.get_rect(center=cannon.center)
+
+        bullet_group.update()
+        
+        # Draw
+        screen.fill((30, 40, 50))
+        screen.blit(rotated_cannon_img, cannon)
+        bullet_group.draw(screen)
+        #print(offset)
+        txt = FONT.render('angle {:.1f}'.format(angle), True, (150, 150, 170))
+        screen.blit(txt, (10, 10))
+        pg.display.update()
+
+        clock.tick(30)
+
+if __name__ == '__main__':
+    main()
+    pg.quit()
