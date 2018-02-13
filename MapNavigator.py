c262d1185534c763210806b88646207f296fbc3a
eliecer <eliecerecology@gmail.com>
2018-02-13 18:36:18 +0200

    loop

--- MapNavigator.py
+++ MapNavigator.py
@@ -83,61 +83,65 @@ def main():
     t = []
     #j = 0
     #while not done:
-    for j in range(0,10):
-        clock.tick(60)
-        #j += 1
-
-        #for j in range(0,10):
-        turn_right(0.1)
-        player.angle_speed = angle[j]#-0.5
-        print(hmc5883l.getHeading())
-        print(hmc5883l.getHeading()*3.14/180, "radians")
-        print(distance(), "distance")
-        angle.append(hmc5883l.getHeading()*3.14/180 + 8.4)
-        dista.append(distance())
-        t.append(j)
-        time.sleep(0.5)
-        
+    for k in range(0, 100):    
+        for j in range(0,15):
+                clock.tick(60)
+                turn_left(0.1)
+                player.angle_speed = angle[j]#-0.5
+                print(hmc5883l.getHeading())
+                print(hmc5883l.getHeading()*3.14/180, "radians")
+                print(distance(), "distance")
+                angle.append(hmc5883l.getHeading()*3.14/180 + 8.4)
+                dista.append(distance())
+                t.append(j)
+                time.sleep(0.5)
+                playersprite.update()
+                screen.fill((30, 30, 30))
+                playersprite.draw(screen)
+                pg.display.flip()
+                
+        print(max(dista), "max distance")       
+            
         playersprite.update()
-
         screen.fill((30, 30, 30))
         playersprite.draw(screen)
         pg.display.flip()
-       
-    '''MANUAL CONTROL
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                done = True
-            elif event.type == pg.KEYDOWN:
-                if event.key == pg.K_UP:
-                    player.speed = 0.3 #SOLO VELOCITY
-                    forward(0.2)
-                    #i = var
-                    #print(int(float(i)))
-                elif event.key == pg.K_DOWN:
-                    player.speed = -0.3
-                    backward(0.3)
-                    #i = var
-                    #print(int(float(i)))
-                elif event.key == pg.K_LEFT:
-                    player.angle_speed = 0.3
-                    turn_left(0.3)
-                elif event.key == pg.K_RIGHT:
-                    player.angle_speed = -0.3 # angle speed is anf
-                    turn_right(0.3)
-            elif event.type == pg.KEYUP:
-                if event.key == pg.K_LEFT:
-                    player.angle_speed = 0
-                elif event.key == pg.K_RIGHT:
-                    player.angle_speed = 0
-                elif event.key == pg.K_UP:
-                    player.speed = 0
-                elif event.key == pg.K_DOWN:
-                    player.speed = 0
-        '''            
         
-    #plt.plot(t, angle)
-    #plt.show()
+            
+        '''MANUAL CONTROL
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    done = True
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_UP:
+                        player.speed = 0.3 #SOLO VELOCITY
+                        forward(0.2)
+                        #i = var
+                        #print(int(float(i)))
+                    elif event.key == pg.K_DOWN:
+                        player.speed = -0.3
+                        backward(0.3)
+                        #i = var
+                        #print(int(float(i)))
+                    elif event.key == pg.K_LEFT:
+                        player.angle_speed = 0.3
+                        turn_left(0.3)
+                    elif event.key == pg.K_RIGHT:
+                        player.angle_speed = -0.3 # angle speed is anf
+                        turn_right(0.3)
+                elif event.type == pg.KEYUP:
+                    if event.key == pg.K_LEFT:
+                        player.angle_speed = 0
+                    elif event.key == pg.K_RIGHT:
+                        player.angle_speed = 0
+                    elif event.key == pg.K_UP:
+                        player.speed = 0
+                    elif event.key == pg.K_DOWN:
+                        player.speed = 0
+            '''            
+            
+        #plt.plot(t, angle)
+        #plt.show()
 if __name__ == '__main__':
     main()
     pg.quit()
